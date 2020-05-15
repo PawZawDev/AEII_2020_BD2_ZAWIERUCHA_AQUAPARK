0ed4cf6c3309b6564c11327125d70a398310beee
PawZaw <49035966+PawZawDev@users.noreply.github.com>
2020-05-15 13:27:06 +0200

    dodane atrakcje

---
+++ Aquapark/Aquapark/Controllers/AttractionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Aquapark.Models;
+
+namespace Aquapark.Controllers
+{
+    public class AttractionsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Attractions
+        public ActionResult Index()
+        {
+            return View(db.Attraction.ToList());
+        }
+
+        // GET: Attractions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attraction attraction = db.Attraction.Find(id);
+            if (attraction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attraction);
+        }
+
+        // GET: Attractions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Attractions/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,IsOpen")] Attraction attraction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Attraction.Add(attraction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(attraction);
+        }
+
+        // GET: Attractions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attraction attraction = db.Attraction.Find(id);
+            if (attraction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attraction);
+        }
+
+        // POST: Attractions/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,IsOpen")] Attraction attraction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(attraction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(attraction);
+        }
+
+        // GET: Attractions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Attraction attraction = db.Attraction.Find(id);
+            if (attraction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(attraction);
+        }
+
+        // POST: Attractions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Attraction attraction = db.Attraction.Find(id);
+            db.Attraction.Remove(attraction);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
