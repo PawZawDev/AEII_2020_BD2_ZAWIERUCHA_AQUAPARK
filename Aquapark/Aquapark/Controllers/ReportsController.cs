9746da58839736bc714b07c3a340816d19e30ca9
Dominik <dominikbis1998@o2.pl>
2020-05-19 18:12:47 +0200

    Added ReportsController

---
+++ Aquapark/Aquapark/Controllers/ReportsController.cs
@@ -0,0 +1,41 @@
+﻿using Aquapark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace Aquapark.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private Entities db = new Entities();
+
+
+        // GET: Reports
+        public ActionResult Index(string selectedChart)
+        {
+            ViewBag.selectedChart = selectedChart;
+            return View();
+        }
+
+        public ActionResult TicketsTypes()
+        {
+
+            var order = db.TicketInPriceList.Select(n => new
+            {
+                y = n.Id,
+                x = n.TicketType.Type
+            });
+
+            ViewBag.data = order.ToList();
+
+            var chart = new Chart(600, 400).AddSeries(name: "Tickets", yValues: order, yFields: "y", xValue: order, xField: "x").AddTitle("Tickets types");
+
+            ViewBag.chart = chart;
+
+            return View("Chart");
+        }
+    }
+}
